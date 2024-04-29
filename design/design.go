a15c2e4481b689a3ad5455a42555d2c9532b8ba7
Akeem Amusat <aoamusat@gmail.com>
2024-04-29 14:04:14 +0100

    Generated Goa DSL

--- design/design.go
+++ design/design.go
@@ -11,22 +11,6 @@ const (
 	GRPC_PORT = 3060
 )
 
-type URLResult struct {
-	LongURL  string `json:"long_url"`
-	ShortURL string `json:"short_url"`
-}
-
-var ShortenedURL = ResultType("ShortenedURL", func() {
-	Description("Shortened URL response")
-	Attribute("long_url", String, "Original Long URL", func() {
-		Example("https://example.com/very/long/url/path")
-	})
-	Attribute("short_url", String, "Shortened URL", func() {
-		Example("https://short.url/xyz123")
-	})
-	Required("short_url", "long_url")
-})
-
 var _ = API("GoTrim API", func() {
 	Title("GoTrim API Service")
 	Description("URL shortening service written in Go")
@@ -40,15 +24,13 @@ var _ = API("GoTrim API", func() {
 
 var _ = Service("create", func() {
 	Description("The create service performs shortening operation on long URL.")
-
 	Method("create", func() {
 		Payload(func() {
 			Field(1, "long_url", String, "Payload for shortening a URL")
 			Required("long_url")
 		})
 
-		Result(ShortenedURL)
-
+		Result(String)
 		HTTP(func() {
 			POST("/api/v1/shorten")
 			Body(func() {
@@ -56,10 +38,8 @@ var _ = Service("create", func() {
 				Required("long_url")
 			})
 		})
-
 		GRPC(func() {
 		})
 	})
-
 	Files("/openapi.json", "./gen/http/openapi.json")
 })
